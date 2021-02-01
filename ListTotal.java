a46d34e8b8a76179e95eb6866b48eeaea4d94ee4
Jonathan Hsing <600020173@fjuhsd.org>
2021-02-01 15:52:52 +0000

    completed ListTotal

--- ListTotal.java
+++ ListTotal.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name -Jonathan Hsing
+//Date -2/1/21
 
 import java.util.List;
 import java.util.ArrayList;
@@ -10,6 +10,15 @@ public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
+  //Create a variable to store the sum
+  int a = 0;
+  //Create a for each loop to loop each value of the array
+  for (int b: ray){
+	//Set sum equal to the sum added to the current array value
+  a += b;
+  //End loop
+ }
+  //Return the sum
+return a;
  }
 }
